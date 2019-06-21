7999183aa3fb10a655e83d205914d2790b4f5c8f
DiegoRamires <goncalves.ramires@gmail.com>
2019-06-21 17:58:44 -0300

    create title component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { storeProducts } from '../data'
 import Product from './Product';
+import Title from './Title'
 
 class ProductList extends Component {
+  state = {
+    products: storeProducts
+  }
+
   render() {
+    console.log(this.state.products)
     return (
-      <div>
+      <>
+        <div className="py-5">
+          <div className="container">
+            <div className="row">
+              <Title name="our" title="products" />
+            </div>
+          </div>
+        </div>
         <Product />
-      </div>
+      </>
     );
   }
 }
