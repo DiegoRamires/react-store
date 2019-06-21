7bad0891a4eda21cef40ef29734b79079b09cedb
DiegoRamires <goncalves.ramires@gmail.com>
2019-06-21 18:40:31 -0300

    passing data into components with context api

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import { storeProducts } from '../data'
+import { ProductConsumer } from '../context'
 import Product from './Product';
 import Title from './Title'
 
 class ProductList extends Component {
-  state = {
-    products: storeProducts
-  }
-
   render() {
-    console.log(this.state.products)
     return (
       <>
         <div className="py-5">
           <div className="container">
             <div className="row">
               <Title name="our" title="products" />
+              <div className="row">
+                <ProductConsumer>
+                  {(value) => {
+                    return value.products.map(product => {
+                      return <Product
+                        key={product.id}
+                        product={product}
+                      />
+                    })
+                  }}
+                </ProductConsumer>
+              </div>
             </div>
           </div>
         </div>
