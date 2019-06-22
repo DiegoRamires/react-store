e06c3a92beb4aa8ee29db4171166651f80a43557
DiegoRamires <goncalves.ramires@gmail.com>
2019-06-22 13:58:02 -0300

    create modal component. showing when add a product in cart with options to go to cart and close modal

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,11 @@ export const ButtonContainer = styled.button`
   text-transform: capitalize;
   font-size: 1.4rem;
   background: transparent;
-  border: 0.05rem solid var(--mainWhite);
+  border: 0.05rem solid var(--lightBlue);
   border-color: ${props =>
-    (props.cart ? "var(--mainYellow)" : "var(--mainWhite)")};
+    (props.cart ? "var(--mainYellow)" : "var(--lightBlue)")};
   color: ${props =>
-    props.cart ? "var(--mainYellow)" : "var(--mainWhite)"};
+    props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
   border-radius: 0.5rem;
   padding: 0.2rem 0.5rem;
   cursor: pointer;
